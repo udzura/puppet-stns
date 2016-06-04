e5727065f4bbe7317612fd8e62908f2cf7f5b795
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-06-04 23:01:59 +0900

    wip

--- manifests/server.pp
+++ manifests/server.pp
@@ -4,14 +4,18 @@
 # stns::server is to install and configure stns.
 
 class stns::server (
-  $port     = 1104,
-  $user     = undef,
-  $password = undef,
+  $port              = 1104,
+  $user              = undef,
+  $password          = undef,
+  $sudoers_password  = undef,
+  $sudoers_hash_type = undef,
 ) {
 
   validate_integer($port)
   validate_string($user)
   validate_string($password)
+  validate_string($sudoers_password)
+  validate_string($sudoers_hash_type)
 
   require stns::repo
 
